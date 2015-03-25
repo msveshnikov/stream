4f0fe28695d61ca49ca3ae808eadd04d4ecc6a96
Max Sveshnikov <msveshnikov@thumbtack.net>
2015-03-25 16:30:55 +0600

    import

--- stream/models.py
+++ stream/models.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 
 from django.db import models
 from django.utils import timezone
@@ -23,3 +24,18 @@ class Choice(models.Model):
     def __unicode__(self):  # __unicode__ on Python 2
         return self.choice_text
 
+
+class Import(object):
+    def do(self):
+        with open("c:\\version.txt") as f:
+            mystring = f.read()
+        mystring_list = [item for item in mystring.split(" ")]
+        for item in mystring_list:
+            try:
+                a = re.search("(?P<url>https?://[^\s]+)", item).group("url")
+                if (a[-3:] == "jpg" or a[-3:] == "gif"):
+                    print(a)
+                    q = Question(question_text=a, pub_date=timezone.now())
+                    q.save()
+            except:
+                pass
